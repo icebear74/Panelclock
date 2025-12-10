c7270c2f5f6a1337accc95dd5f2b057db34556a3
Holger Rojahn <iceholger@gmail.com>
2025-12-11 00:21:25 +0100

    Added Write Version Script

--- Version.hpp
+++ Version.hpp
@@ -2,7 +2,7 @@
 #define VERSION_HPP
 
 // Panelclock Firmware Version
-#define PANELCLOCK_VERSION "1.0.0.8be6818"
+#define PANELCLOCK_VERSION "1.0.0-24f3daa35086cbb98c78fa922a2c8de4f3dd95ed"
 #define PANELCLOCK_BUILD_DATE __DATE__
 #define PANELCLOCK_BUILD_TIME __TIME__
 
